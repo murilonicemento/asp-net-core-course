aa047b8b268e6625b96c48030edd6b59e4a8e903
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-31 23:07:05 -0300

    Routing

--- Routing/MyFirstApp/MyFirstApp/Program.cs
+++ Routing/MyFirstApp/MyFirstApp/Program.cs
@@ -1,24 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    Endpoint endpoint = context.GetEndpoint();
-    await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
-    await next(context);
-});
+// app.Use(async (context, next) =>
+// {
+//     Endpoint endpoint = context.GetEndpoint();
+//     await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+//     await next(context);
+// });
 
 // Habilita roteamento
 app.UseRouting();
 
-app.Use(async (context, next) =>
-{
-    Endpoint? endpoint = context.GetEndpoint();
-    await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
-    await next(context);
-});
+// app.Use(async (context, next) =>
+// {
+//     Endpoint? endpoint = context.GetEndpoint();
+//     await context.Response.WriteAsync($"Endpoint: {endpoint?.DisplayName}\n");
+//     await next(context);
+// });
 
 // Cria os end points
+
+#pragma warning disable ASP0014
 app.UseEndpoints(endpoints =>
 {
     // Adiciona os end points
@@ -30,6 +32,117 @@ app.UseEndpoints(endpoints =>
     endpoints.MapPost("/map2", async (context) =>
     {
         await context.Response.WriteAsync("Hello map2");
+    });
+
+    endpoints.Map("files/{filename}.{extension}", async (context) =>
+    {
+        string? filename = Convert.ToString(context.Request.RouteValues["filename"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+        await context.Response.WriteAsync($"In file {filename} and have the {extension} extension");
+    });
+    
+    // endpoints.Map("employee/profile/{employeename}", async (context) =>
+    // {
+    //     string? employee = context.Request.RouteValues["employeename"].ToString();
+    //     await context.Response.WriteAsync($"Employee name is {employee}");
+    // });
+    
+    // Default Parameters
+    // endpoints.Map("/employee/profile/{employeename=ronaldo}", async (context) =>
+    // {
+    //     string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+    //     await context.Response.WriteAsync($"Employee name is {employee}");
+    // });
+    
+    // Optional Parameters
+    // endpoints.Map("/products/details/{id?}", async (context) =>
+    // {
+    //     int? id = Convert.ToInt32(context.Request?.RouteValues["id"]);
+    //     await context.Response.WriteAsync($"Product id is {id}");
+    // });
+    
+    // Route constraints
+    endpoints.Map("/products/details/{id:int?}", async (context) =>
+    {
+        int? id = Convert.ToInt32(context.Request?.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product id is {id}");
+    });
+    
+    endpoints.Map("daily-digest-report/{reportdate:datatime}", async (context) =>
+    {
+        DateTime? reportDate = Convert.ToDateTime(context.Request?.RouteValues["datetime"]);
+        await context.Response.WriteAsync($"Product id is {reportDate.Value.ToShortDateString()}");
+    });
+    
+    // Global Universal Identifier
+    endpoints.Map("cities/{cityid:guid}", async context =>
+    {
+        Guid? id = Guid.Parse(context.Request?.RouteValues["cityid"]?.ToString()!);
+
+        await context.Response.WriteAsync($"GUID id is - {id}");
+    });
+    
+    endpoints.Map("cities/{cityid:guid}", async context =>
+    {
+        Guid? id = Guid.Parse(context.Request?.RouteValues["cityid"]?.ToString()!);
+
+        await context.Response.WriteAsync($"GUID id is - {id}");
+    });
+    
+    // tamanho mínino
+    endpoints.Map("/employee/profile/{employeename:minlength(4)}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
+    });
+    
+    // tamanho máximo
+    endpoints.Map("/employee/profile/{employeename:maxlength(10)}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
+    });
+    
+    // tamanho minino e máximo
+    endpoints.Map("/employee/profile/{employeename:minlength(4):maxlength(7)}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
+    });
+    
+    // tamanho minino e máximo
+    endpoints.Map("/employee/profile/{employeename:length(4, 10)}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
+    });
+    
+    // valor mínimo
+    endpoints.Map("/employee/profile/{employeename:min(1)}", async (context) =>
+    {
+        int? id = Convert.ToInt32(context.Request?.RouteValues["employeename"]);
+        await context.Response.WriteAsync($"Employee id is {id}");
+    });
+    
+    // valor máximo
+    endpoints.Map("/employee/profile/{employeename:max(999999)}", async (context) =>
+    {
+        int? id = Convert.ToInt32(context.Request?.RouteValues["employeename"]);
+        await context.Response.WriteAsync($"Employee id is {id}");
+    });
+    
+    // Aceita somente caracteres alfabéticos
+    endpoints.Map("/employee/profile/{employeename:alpha}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
+    });
+    
+    // regex match (só aceita palavras john, maria e ronaldo)
+    endpoints.Map("/employee/profile/{employeename:regex(^(john|maria|ronaldo)$)}", async (context) =>
+    {
+        string? employee = context.Request?.RouteValues["employeename"]?.ToString();
+        await context.Response.WriteAsync($"Employee name is {employee}");
     });
 });
 
--- Routing/MyFirstApp/MyFirstApp/obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
+++ Routing/MyFirstApp/MyFirstApp/obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a9bb5fe9feb62ebc9b40280db44527f95a40544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c3a38225fd3bbefa01a78b3381433db05c8674")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
