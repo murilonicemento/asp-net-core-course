62a3287cef0fe2b879d39e590f8d2c3452493fb4
murilonicemento <murilo_nascimento22@outlook.com>
2024-08-25 10:52:36 -0300

    model validations

--- ModelBinding-And-Validations/Validations/Validations/Models/Person.cs
+++ ModelBinding-And-Validations/Validations/Validations/Models/Person.cs
@@ -4,12 +4,15 @@ namespace Validations.Models;
 
 public class Person
 {
-  [Required(ErrorMessage = "Person Name can not be empty or null.")]
+  [Required(ErrorMessage = "{0} can not be empty or null.")]
+  [Display(Name = "Person Name")]
+  [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
   public string? PersonName { get; set; }
   public string? Email { get; set; }
   public string? Phone { get; set; }
   public string? Password { get; set; }
   public string? ConfirmPassword { get; set; }
+  [Range(0, 999.9, ErrorMessage = "{0} should be between {1} and {2}")]
   public double? Price { get; set; }
 
   public override string ToString()
