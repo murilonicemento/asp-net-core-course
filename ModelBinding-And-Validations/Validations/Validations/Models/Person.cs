91a1dd71aeaae4ddae531f7c9a58bc8a394d06d2
murilonicemento <murilo_nascimento22@outlook.com>
2024-08-25 11:03:38 -0300

    model validations

--- ModelBinding-And-Validations/Validations/Validations/Models/Person.cs
+++ ModelBinding-And-Validations/Validations/Validations/Models/Person.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Validations.Models;
 
@@ -7,11 +8,25 @@ public class Person
   [Required(ErrorMessage = "{0} can not be empty or null.")]
   [Display(Name = "Person Name")]
   [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
+  [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should be contain only alphabets, space and dot (.).")]
   public string? PersonName { get; set; }
+
+  [EmailAddress(ErrorMessage = "{0} should be a proper email address.")]
+  [Required(ErrorMessage = "{0} can not be blank.")]
   public string? Email { get; set; }
+
+  [Phone(ErrorMessage = "{0} should contain 10 digits.")]
+  // [ValidateNever]
   public string? Phone { get; set; }
+
+  [Required(ErrorMessage = "{0} can not be blank.")]
   public string? Password { get; set; }
+
+  [Required(ErrorMessage = "{0} can not be blank.")]
+  [Compare("Password", ErrorMessage = "{0} and {1} do not match.")]
+  [Display(Name = "Re-enter Password")]
   public string? ConfirmPassword { get; set; }
+
   [Range(0, 999.9, ErrorMessage = "{0} should be between {1} and {2}")]
   public double? Price { get; set; }
 
