316e1b011c0e453ba7b6ca51d11bbfddad28f8ee
murilonicemento <murilo_nascimento22@outlook.com>
2024-09-21 17:46:56 -0300

    switch case

--- RazorViews/ViewsExample/ViewsExample/Models/Person.cs
+++ RazorViews/ViewsExample/ViewsExample/Models/Person.cs
@@ -4,4 +4,13 @@ public class Person
 {
     public string? Name { get; set; }
     public DateTime? DateOfBirth { get; set; }
+
+    public Gender PersonGender { get; set; }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
 }
