0b712693fb310c1e2db64e31a99b1017995fb6c0
murilonicemento <murilo_nascimento22@outlook.com>
2024-09-22 12:49:37 -0300

    strongly typed views

--- RazorViews/ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ RazorViews/ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ViewsExample.Models;
+
 namespace ViewsExample.Controllers;
 
 public class HomeController : Controller
@@ -32,8 +33,41 @@ public class HomeController : Controller
         };
         // ViewData["people"] = people;
         ViewBag.people = people;
-        return View(); // Index.cshtml
+        return View(people); // Index.cshtml
         // return View("abc"); // abc.cshtml
         // return new ViewResult() { ViewName = "abc" };
+    }
+
+    [Route("person-details/{name}")]
+    public IActionResult Details(string? name)
+    {
+        if (name is null)
+            return Content("Person name can't be null");
+
+        List<Person> people = new List<Person>()
+        {
+            new Person()
+            {
+                Name = "Dêcio",
+                DateOfBirth = DateTime.Now,
+                PersonGender = Person.Gender.Male
+            },
+            new Person()
+            {
+                Name = "Ronaldo",
+                DateOfBirth = DateTime.Now,
+                PersonGender = Person.Gender.Other
+            },
+            new Person()
+            {
+                Name = "Maria",
+                DateOfBirth = DateTime.Now,
+                PersonGender = Person.Gender.Female
+            }
+        };
+
+        Person? matchingPerson = people.Where(temp => temp.Name == name).FirstOrDefault();
+
+        return View(matchingPerson);
     }
 }
