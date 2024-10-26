1fa4de4584ac0941cc5a1375383a045494fdb2f9
murilonicemento <murilo_nascimento22@outlook.com>
2024-10-26 16:23:08 -0300

    implementation GetSortedPersons method

--- xUnit/ContactManager/Services/PersonsService.cs
+++ xUnit/ContactManager/Services/PersonsService.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using Entities;
 using ServiceContracts;
 using ServiceContracts.DTO;
@@ -103,8 +102,48 @@ public class PersonsService : IPersonsService
         return matchingPersons;
     }
 
-    public List<PersonResponse> GetSortedPersons(List<PersonResponse> allPersons, string sortedBy, SortOrderOptions sortOrder)
+    public List<PersonResponse> GetSortedPersons(List<PersonResponse> allPersons, string sortedBy,
+        SortOrderOptions sortOrder)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(sortedBy)) return allPersons;
+
+        List<PersonResponse> personResponses = (sortedBy, sortOrder) switch
+        {
+            (nameof(PersonResponse.Name), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Name, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Name), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Name, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Email), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Email), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.DateOfBirth), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.DateOfBirth).ToList(),
+            (nameof(PersonResponse.DateOfBirth), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.DateOfBirth).ToList(),
+            (nameof(PersonResponse.Age), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Age).ToList(),
+            (nameof(PersonResponse.Age), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Age).ToList(),
+            (nameof(PersonResponse.Gender), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Gender), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Country), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Country), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Address), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Address), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.ASC) => allPersons
+                .OrderBy(person => person.ReceiveNewsLetters).ToList(),
+            (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.DESC) => allPersons
+                .OrderByDescending(person => person.ReceiveNewsLetters).ToList(),
+            _ => allPersons
+        };
+
+        return personResponses;
     }
 }
--- xUnit/ContactManager/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ xUnit/ContactManager/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3b34fd20363dec529109e64bbd13f505698772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094388a8f94d7362bf0303ba8664fd05104f041d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
