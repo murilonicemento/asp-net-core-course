e15aec5fc49b9d78aafd77d035e76fc7e655684f
murilonicemento <murilo_nascimento22@outlook.com>
2024-12-12 23:25:49 -0300

    role based authentication

--- identity-authorization-security/ContactManager/ContactsManager.UI/Areas/Admin/Controllers/HomeController.cs
+++ identity-authorization-security/ContactManager/ContactsManager.UI/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Authorize(Roles = "Admin")]
 public class HomeController : Controller
 {
     [Route("admin/home/index")]
